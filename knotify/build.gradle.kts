9874ab197a300a7c85f940916773e7ed87e89863
elyahou <elyahou.hadass@gmail.com>
2024-10-06 20:17:19 +0300

    publish to maven

--- knotify/build.gradle.kts
+++ knotify/build.gradle.kts
@@ -1,8 +1,8 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.multiplatform)
-    id("convention.publication")
-    `maven-publish`
-
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 
@@ -14,9 +14,7 @@ kotlin {
 
     sourceSets {
         jvmMain.dependencies {
-            implementation(libs.slf4jApi)
-            implementation(libs.logbackClassic)
-
+            implementation("io.github.kdroidfilter:kmplog:0.1.0")
         }
         jvmTest.dependencies {
             implementation(kotlin("test"))
@@ -33,13 +31,50 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["kotlin"])
-            groupId = "com.github.kdroidFilter"
-            artifactId = "KNotify"
-            version = "0.1.2"
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.kdroidfilter",
+        artifactId = "knotify",
+        version = version.toString()
+    )
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("KNotify")
+        description.set("KNotify is a Kotlin library for sending native notifications across different operating systems. It provides a simple, unified interface for sending notifications on Linux, Windows, and macOS.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/kdroidFilter/KNotify")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("kdroidfilter")
+                name.set("Elyahou Hadass")
+                email.set("elyahou.hadass@gmail.com")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/kdroidFilter/KNotify")
         }
     }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
+
+
+task("testClasses") {}
