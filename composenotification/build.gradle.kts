3746f9e8976b7790c66778cb9d91257a26113d90
Elie G <myriam.hadass@gmail.com>
2024-10-21 10:43:51 +0300

    Refactor SDK configuration and rename AndroidManifest
    Updated testOptions.targetSdk in build.gradle.kts for clarity. Renamed and streamlined AndroidManifest.xml to reflect proper directory structure and removed unnecessary comments.

--- composenotification/build.gradle.kts
+++ composenotification/build.gradle.kts
@@ -72,7 +72,7 @@ android {
 
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        testOptions.targetSdk = libs.versions.android.targetSdk.get().toInt()
     }
 }
 
